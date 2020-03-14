a8771d0d6b70d11ee8ff223bc9b99239318b9192
Jaime Bisuna <jaimebisona@gmail.com>
2020-03-15 03:20:59 +0800

    updated bug

--- lib/maps_corona.dart
+++ lib/maps_corona.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:ffi';
 
 import 'package:coronatracker/models/results.dart';
@@ -26,6 +27,7 @@ class MapsCorona extends StatefulWidget {
 class _MapsCoronaState extends State<MapsCorona> {
   LatLng initialPos = LatLng(12, 121);
   bool didTap = false;
+  bool loading = false;
   Placemark initialPm = Placemark();
   MapController controller = MapController();
   Country initialCountry;
@@ -33,7 +35,6 @@ class _MapsCoronaState extends State<MapsCorona> {
 
   getMarkers(Country country) {
     widget.placemarks.forEach((placemark) {
-      print(placemark.country);
       if (placemark.country.contains(country.countryName)) {
         markerList.add(
           Marker(
@@ -63,11 +64,11 @@ class _MapsCoronaState extends State<MapsCorona> {
                 child: Icon(
                   Icons.location_on,
                   color:
-                      int.parse(country.info.totalCases.replaceAll(',', '')) >
-                              50
+                      int.parse(country.info.totalCases.replaceAll(',', '')) >=
+                              10
                           ? Colors.redAccent[100]
                           : Colors.greenAccent[100],
-                  size: 30.0,
+                  size: 40.0,
                 ),
               );
             },
@@ -78,8 +79,22 @@ class _MapsCoronaState extends State<MapsCorona> {
   }
 
   loadData() {
+    // Timer.run(() {
+    //   setState(() {
+    //     loading = true;
+    //   });
+    // });
     widget.country.forEach((ctry) {
       getMarkers(ctry);
+    });
+    Future<MapController> mc = controller.onReady;
+    mc.whenComplete(() {
+      print('complete');
+      setState(() {
+        loading = false;
+      });
+    }).catchError((onError) {
+      print(onError);
     });
   }
 
@@ -88,12 +103,21 @@ class _MapsCoronaState extends State<MapsCorona> {
     // TODO: implement initState
     super.initState();
     loadData();
+    print(controller.ready);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              print('reload');
+            },
+          )
+        ],
         centerTitle: true,
         backgroundColor: Color(0xff102044),
         title: Column(
@@ -122,138 +146,174 @@ class _MapsCoronaState extends State<MapsCorona> {
         ),
       ),
       body: Container(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 4,
-              child: Container(
-                child: FlutterMap(
-                  mapController: controller,
-                  options: new MapOptions(
-                    center: initialPos,
-                    zoom: 2.0,
-                    minZoom: 2.0,
-                    interactive: true,
-                    debug: true,
-                  ),
-                  layers: [
-                    TileLayerOptions(
-                      urlTemplate:
-                          'https://tile.jawg.io/dark/{z}/{x}/{y}.png?api-key=community',
-                      subdomains: ['a', 'b', 'c'],
-                    ),
-                    MarkerLayerOptions(
-                      markers: markerList,
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                width: double.infinity,
-                color: Color(0xff000D29),
-                child: !didTap
-                    ? Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
+        color: Color(0xff343332),
+        child: loading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 4,
+                    child: Container(
+                      child: Stack(
                         children: <Widget>[
-                          Icon(
-                            Icons.location_on,
-                            color: Colors.red,
-                            size: 40.0,
+                          FlutterMap(
+                            mapController: controller,
+                            key: Key('maps'),
+                            options: new MapOptions(
+                                center: initialPos,
+                                zoom: 1.5,
+                                minZoom: 1.5,
+                                interactive: true,
+                                debug: true,
+                                onPositionChanged: (pos, b) {
+                                  setState(() {
+                                    initialPos = pos.center;
+                                  });
+                                }),
+                            layers: [
+                              TileLayerOptions(
+                                backgroundColor: Color(0xff191a1a),
+                                urlTemplate:
+                                    'https://tile.jawg.io/dark/{z}/{x}/{y}.png?api-key=community',
+                                subdomains: ['a', 'b', 'c'],
+                              ),
+                              MarkerLayerOptions(
+                                markers: markerList,
+                              )
+                            ],
                           ),
-                          Text(
-                            'Tap on a marker',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30.0,
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                right: 5.0,
+                                bottom: 5.0,
+                              ),
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.my_location,
+                                  size: 30.0,
+                                ),
+                                color: Colors.white,
+                                onPressed: () {
+                                  setState(() {
+                                    controller.move(initialPos, 2);
+                                  });
+                                },
+                              ),
                             ),
-                          ),
+                          )
                         ],
-                      )
-                    : Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                              vertical: 10.0,
-                            ),
-                            child: Row(
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      width: double.infinity,
+                      color: Color(0xff000D29),
+                      child: !didTap
+                          ? Row(
                               mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
                               children: <Widget>[
                                 Icon(
                                   Icons.location_on,
                                   color: Colors.red,
+                                  size: 40.0,
                                 ),
                                 Text(
-                                  '${initialCountry.countryName}',
+                                  'Tap on a marker',
                                   style: TextStyle(
                                     color: Colors.white,
                                     fontWeight: FontWeight.bold,
-                                    fontSize: 25.0,
+                                    fontSize: 30.0,
                                   ),
                                 ),
                               ],
+                            )
+                          : Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 10.0,
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.location_on,
+                                        color: Colors.red,
+                                      ),
+                                      Text(
+                                        '${initialCountry.countryName}',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 25.0,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    TotalCases(
+                                      data: initialCountry.info.totalCases,
+                                      dataSize: 20,
+                                      type: 'Total Cases',
+                                      isMaps: true,
+                                      textSize: 12,
+                                    ),
+                                    TotalRecovered(
+                                      data: initialCountry.info.totalRecovered,
+                                      dataSize: 20,
+                                      type: 'Total Recovered',
+                                      textSize: 12,
+                                    ),
+                                    ActiveCases(
+                                      data: initialCountry.info.activeCases,
+                                      dataSize: 20,
+                                      type: 'Active',
+                                      textSize: 12,
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 5.0,
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    ActiveCases(
+                                      data: initialCountry.info.newDeaths,
+                                      type: 'New Deaths',
+                                      dataSize: 20,
+                                      textSize: 12,
+                                    ),
+                                    TotalDeaths(
+                                      data: initialCountry.info.totalDeaths,
+                                      type: 'Total Deaths',
+                                      dataSize: 20,
+                                      textSize: 12,
+                                      isMaps: true,
+                                    ),
+                                    ActiveCases(
+                                      data: initialCountry.info.seriousCritical,
+                                      type: 'Serious',
+                                      dataSize: 20,
+                                      textSize: 12,
+                                    )
+                                  ],
+                                ),
+                              ],
                             ),
-                          ),
-                          Row(
-                            children: <Widget>[
-                              TotalCases(
-                                data: initialCountry.info.totalCases,
-                                dataSize: 20,
-                                type: 'Total Cases',
-                                isMaps: true,
-                                textSize: 12,
-                              ),
-                              TotalRecovered(
-                                data: initialCountry.info.totalRecovered,
-                                dataSize: 20,
-                                type: 'Total Recovered',
-                                textSize: 12,
-                              ),
-                              ActiveCases(
-                                data: initialCountry.info.activeCases,
-                                dataSize: 20,
-                                type: 'Active',
-                                textSize: 12,
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 5.0,
-                          ),
-                          Row(
-                            children: <Widget>[
-                              ActiveCases(
-                                data: initialCountry.info.newDeaths,
-                                type: 'New Deaths',
-                                dataSize: 20,
-                                textSize: 12,
-                              ),
-                              TotalDeaths(
-                                data: initialCountry.info.totalDeaths,
-                                type: 'Total Deaths',
-                                dataSize: 20,
-                                textSize: 12,
-                                isMaps: true,
-                              ),
-                              ActiveCases(
-                                data: initialCountry.info.seriousCritical,
-                                type: 'Serious',
-                                dataSize: 20,
-                                textSize: 12,
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
       ),
     );
   }
